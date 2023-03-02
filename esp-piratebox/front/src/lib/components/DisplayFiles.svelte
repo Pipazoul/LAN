02c73edd2e77cc971ddd06de9ddd7ca3e8691867
Yassin Siuoda <yassin@siouda.com>
2023-03-02 09:59:22 +0100

    fix: do not load all images to reduce esp load

--- esp-piratebox/front/src/lib/components/DisplayFiles.svelte
+++ esp-piratebox/front/src/lib/components/DisplayFiles.svelte
@@ -7,6 +7,8 @@ let files = []
 let images = [];
 let otherFiles = [];
 
+let currentImage = 0;
+
 onMount(async () => {
     // fetch files from server /files
     fetch('/list').then(res => res.json())
@@ -29,12 +31,17 @@ $: files.forEach(file => {
     <h1>File list</h1>
     <h1>Images</h1>
     <ul>
-        
-        {#each images as file}
-            <li>
-                <img src="/public/{file.name}" alt="{file.name}" />
-            </li>
-        {/each}
+        {#if images.length > 0}
+            <img src="/public/{images[currentImage].name}" alt="d" />
+            <button on:click={() => currentImage = currentImage === images.length - 1 ? 0 : currentImage + 1}>Next</button>
+            <button on:click={() => currentImage = currentImage === 0 ? images.length - 1 : currentImage - 1}>Previous</button>
+            {#each images as image, i}
+                <ul>
+                    <li on:click={() => currentImage = i}>{image.name}</li>
+                </ul>
+            {/each}
+        {/if}
+     
     </ul>
     <h1>Autres fichiers</h1>
     <ul>
